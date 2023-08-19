b98804010608720583eabe367b7f9a0ed73c2237
Sahaj <sahajbajaj2001@gmail.com>
2023-08-19 19:55:47 +1200

    update config

--- Webproject/vite.config.js
+++ Webproject/vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sahajbajaj.github.io/',
+  base: '/sahajbajaj.github.io/Webproject',
   plugins: [vue()],
 })
