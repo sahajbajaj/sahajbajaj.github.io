edfa5885a3496b1ad6136dedd46fee0d6a86f1fe
sahajbajaj <83523552+sahajbajaj@users.noreply.github.com>
2023-08-21 02:05:52 +1200

    Adding Allure reporting to display results

--- Testing/selenium-webproject/src/test/java/automationtests/TodoNgTest.java
+++ Testing/selenium-webproject/src/test/java/automationtests/TodoNgTest.java
@@ -3,10 +3,13 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pageobjects.todopage.Category;
 import pageobjects.todopage.TodoPageObject;
+import io.qameta.allure.testng.AllureTestNg;
 
+@Listeners(AllureTestNg.class)
 public class TodoNgTest {
     TodoPageObject todoPage;
 
